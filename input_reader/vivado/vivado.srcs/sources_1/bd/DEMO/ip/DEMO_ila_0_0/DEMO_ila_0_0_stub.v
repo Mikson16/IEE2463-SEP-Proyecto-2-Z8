2d24ab2a7aa87df757548603a6569eb380551dc8
RodrigoElgueta <rodrigo.elgueta@uc.cl>
2025-06-12 22:15:15 -0400

    interrupciones de sensor de luz

---
+++ input_reader/vivado/vivado.srcs/sources_1/bd/DEMO/ip/DEMO_ila_0_0/DEMO_ila_0_0_stub.v
@@ -0,0 +1,22 @@
+// Copyright 1986-2020 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2020.1 (win64) Build 2902540 Wed May 27 19:54:49 MDT 2020
+// Date        : Thu Jun 12 20:14:16 2025
+// Host        : Ro running 64-bit major release  (build 9200)
+// Command     : write_verilog -force -mode synth_stub
+//               d:/proyectosep2/IEE2463-SEP-Proyecto-2-Z8/input_reader/vivado/vivado.srcs/sources_1/bd/DEMO/ip/DEMO_ila_0_0/DEMO_ila_0_0_stub.v
+// Design      : DEMO_ila_0_0
+// Purpose     : Stub declaration of top-level module interface
+// Device      : xc7z010clg400-1
+// --------------------------------------------------------------------------------
+
+// This empty module with port declaration file causes synthesis tools to infer a black box for IP.
+// The synthesis directives are for Synopsys Synplify support to prevent IO buffer insertion.
+// Please paste the declaration into a Verilog source file or add the file as an additional source.
+(* x_core_info = "ila,Vivado 2020.1" *)
+module DEMO_ila_0_0(clk, probe0, probe1)
+/* synthesis syn_black_box black_box_pad_pin="clk,probe0[0:0],probe1[0:0]" */;
+  input clk;
+  input [0:0]probe0;
+  input [0:0]probe1;
+endmodule
